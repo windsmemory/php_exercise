280f566f06222ef540c9d5e94853d10a12462a34
windsmemory <100366338+windsmemory@users.noreply.github.com>
2022-03-16 23:08:00 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -2,14 +2,87 @@
 
 <head>
 
+<title>墾丁三日遊表單</title>
+<link rel="icon" href="https://images.vocus.cc/8b7c262c-4514-412d-9b4b-c0e4ea1cedf5.jpg" type="image/x-icon" />
+
 </head>
 
-<body>
-<?php
-echo "<h1><b>HELLO PHP !!!</b></h1>";
-$x = 100 , $y = 220;
-echo $x+$y; 
-?>
+<body bgcolor="#F7AFD3">
+<center><h1><p>1.行程內容</p></h1>
+<h2><br>第一天</br></h2>
+<h3><br><font color="#FF0000">早上</font>&emsp;高鐵左營租車 → 車城福安宮 → 古城門(白羊道柴燒麻糬/阿嘉的家)</br></h3>
+<h3><br><font color="#FF0000">晚上</font>&emsp;白沙灣 → 關山看夕陽 → 出火→ 晚上逛墾丁大街夜市 (→ 鑾鼻燈塔夜遊 → 龍磐公園觀星)</br></h3>
+<h2><br>第二天</br></h2>
+<h3><br><font color="#FF0000">早上</font>&emsp;水上活動 (浮潛、乘坐香蕉船、水上摩托車…等)→ 南灣 → 輝哥生魚片</br></h3>
+<h3><br><font color="#FF0000">中午</font>&emsp;下午參加後壁湖浮潛 →貓鼻頭 → 墾丁大街</br></h3>
+<h2><br>第三天</br></h2>
+<h3><br><font color="#FF0000">早上</font>&emsp;船帆石→ 貝殼砂展示館→ 路過燈塔→ 龍磐草原→ 風吹砂</br></h3>
+<h3><br><font color="#FF0000">中午</font>&emsp;打包行李趕路回家。</br></h3>
+<img src="https://i0.wp.com/boo2k.com/wp-content/uploads/2021/06/IMG_0871.jpg?fit=860%2C573&ssl=1">
+<img src="https://i2.wp.com/blog.kkday.com/wp-content/uploads/batch_shutterstock_767865031-2.jpg">
+<h1><p>2.表單填寫</p></h1>
+<form action="/htdocs/form" method="post">
+<h2>名字:&emsp;<input type="text" placeholder="請輸入名字" required></h2>
+<h2>email:&emsp;<input type="email" placeholder="請輸入email" required></h2>
+<h2>電話:&emsp;<input type="text" placeholder="請輸入電話" required></h2>
+<h2>性別:&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;</h2><input type="radio" name="gender" value="男生"> 男生<br>
+<input type="radio" name="gender" value="女生"> 女生<br>
+<h2>葷素:&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;</h2><input type="checkbox" name="food" value="葷">葷<br>
+<input type="checkbox" name="food" value="素">素<br>
+<h2>衣服大小:&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<h2>
+<select>
+    <option>請選擇衣服大小</option>
+    <option>S</option>
+    <option>M</option>
+    <option>L</option>
+    <option>XL</option>
+    <option>XXL</option>
+</select><br>
+<h2>衣服顏色:&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;</h2><input type="color" value="#ff0000"><br>
+<h2>生日:&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;</h2><input type="date"><br>
+<h2>票數:&emsp;<input type="text" placeholder="請輸入票數" required></h2>
+<table>
+    <tr>
+        <th>&emsp;</th>
+        <th>S</th>
+        <th>M</th>
+        <th>L</th>
+        <th>XL</th>
+        <th>XXL</th>
+    </tr>
+    <tr>
+        <td>腰圍/cm</td>
+        <td>62-66</td>
+        <td>67-70</td>
+        <td>71-74</td>
+        <td>75-78</td>
+        <td>79-82</td>
+    </tr>
+    <tr>
+        <td>肩寬/cm</td>
+        <td>37</td>
+        <td>38</td>
+        <td>39</td>
+        <td>40</td>
+        <td>41</td>
+    </tr>
+    <tr>
+        <td>身高/cm</td>
+        <td>155</td>
+        <td>160</td>
+        <td>165</td>
+        <td>170</td>
+        <td>175</td>
+    </tr>
+</table>
+
+<input type="submit" value="送出資料">
+<input type="reset" value="重設">
+</form>
+
+
+</center>
+
 </body>
 
 </html>
